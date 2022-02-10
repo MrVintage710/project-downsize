4e84ee44c8dbe17126e17f45f4f37bb2a3c7d331
Brooks Palin <brookspalin710@gmail.com>
2022-02-10 13:18:11 -0700

    initial commit to texture branch

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 mod render;
 mod util;
 
-use crate::render::*;
+use glow::*;
+use crate::render::{createGlutinContext, buffer::VBO, Renderable};
 use cgmath::Vector3;
-use crate::render::buffer::{VBO, VAO};
+use crate::render::buffer::VAO;
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::ControlFlow;
 use crate::render::shader::ShaderProgram;
-use egui_glow::glow::HasContext;
-use anymap::AnyMap;
-use crate::render::generic::{GenericDrawable, GenericDebug};
+use std::time::SystemTime;
 
-fn main() {
-    let mut clear_color = [0.1, 0.1, 0.1];
+fn main() -> Result<(), String> {
+    let (gl, shader_version, window, event_loop) = createGlutinContext("Hello Triangle!");
 
-    let (window, event_loop) = render::createGlutinContext("Test");
-    let mut render_context = RenderContext::new(&window, &event_loop);
-
-    let gl = &render_context.gl;
+    let mut egui_glow = egui_glow::EguiGlow::new(&window, &gl);
 
     let verts : Vec<Vector3<f32>> = vec![
         Vector3::new(-0.5, 0.5, 0.0),
@@ -33,59 +29,72 @@ fn main() {
     vao.addIndexBuffer(&gl, vec![0, 2, 1, 1, 2, 3]);
     vao.add_vbo(&gl ,0, &vbo);
 
-    let mut program = ShaderProgram::new(&gl).expect("Shader unable to be Created.");
+    let mut program = ShaderProgram::new(&gl)?;
     program.load_vertex_shader(&gl, "static_vert.glsl");
     program.load_fragment_shader(&gl, "static_frag.glsl");
     program.link(&gl);
 
-    let generic_drawable = GenericDrawable::new(move |gl| unsafe {
-        program.bind(gl);
-        vao.render(gl);
-    });
+    program.create_uniform_vec3(&gl, "color_shift", Vector3::new(-0.1, -0.1, -0.1));
 
-    let generic_debug = GenericDebug::new(|ui| {
-        ui.label("Test");
-    });
+    program.bind(&gl);
+    vao.bind(&gl);
 
-    let rendergroup = RenderGroup::new("Test group".to_string(), 0)
-        .with_drawable(generic_drawable)
-        .with_debugable(generic_debug);
+    let start = SystemTime::now();
+    let mut last = SystemTime::now();
 
-    render_context.add_render_group(rendergroup);
+    let mut clear_color = [0.1, 0.1, 0.1];
 
     event_loop.run(move |event, _, control_flow| {
-        let mut redraw = || {
-                render_context.render();
-                render_context.debug(&window);
-                window.swap_buffers().unwrap();
-        };
+        let (test, list) = egui_glow.run(window.window(), |egui_ctx| {
+            egui::SidePanel::left("side_panel").show(egui_ctx, |ui| {
+                ui.heading("Hello World!");
+                if ui.button("Quit").clicked() {
+                    println!("Quit")
+                }
+            });
+        });
 
         match event {
-            // Platform-dependent event handlers to workaround a winit bug
-            // See: https://github.com/rust-windowing/winit/issues/987
-            // See: https://github.com/rust-windowing/winit/issues/1619
-            glutin::event::Event::RedrawEventsCleared if cfg!(windows) => redraw(),
-            glutin::event::Event::RedrawRequested(_) if !cfg!(windows) => redraw(),
+            Event::NewEvents(_) => {}
+            Event::WindowEvent { ref event, .. } => {
+                egui_glow.on_event(event);
+                match event {
+                    WindowEvent::Resized(physical_size) => {
+                        window.resize(*physical_size);
+                        unsafe { gl.viewport(0, 0, physical_size.width as i32, physical_size.height as i32); }
+                    }
+                    WindowEvent::CloseRequested => {
+                        unsafe {
+                            *control_flow = ControlFlow::Exit
+                        }
 
-            glutin::event::Event::WindowEvent { event, .. } => {
-                use glutin::event::WindowEvent;
-                if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    }
+                    _ => (),
                 }
+            },
+            Event::DeviceEvent { .. } => {}
+            Event::UserEvent(_) => {}
+            Event::Suspended => {}
+            Event::Resumed => {}
+            Event::MainEventsCleared => {
+                window.window().request_redraw()
+            }
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    let total_time = start.elapsed().expect("Timing error").as_secs_f32();
+                    program.uniform_vec3(&gl, "color_shift", Vector3::new(total_time.sin(), total_time.sin(), total_time.sin()));
 
-                if let glutin::event::WindowEvent::Resized(physical_size) = event {
-                    window.resize(physical_size);
+                    gl.clear(glow::COLOR_BUFFER_BIT);
+                    vao.pre_render(&gl);
+                    vao.render(&gl);
+                    egui_glow.paint(&window, &gl, list);
+                    window.swap_buffers().unwrap();
                 }
-
-                render_context.on_event(&event);
-
-                window.window().request_redraw(); // TODO: ask egui if the events warrants a repaint instead
             }
-            glutin::event::Event::LoopDestroyed => {
-                render_context.destroy()
-            }
-
-            _ => (),
+            Event::RedrawEventsCleared => {}
+            Event::LoopDestroyed => {}
         }
     });
+
+    Ok(())
 }
--- src/render.rs
+++ src/render.rs
@@ -2,230 +2,51 @@ use glow::{Context, HasContext, FRAMEBUFFER_SRGB};
 use glutin::window::Window;
 use glutin::event_loop::EventLoop;
 use glutin::{ContextWrapper, PossiblyCurrent, WindowedContext};
-use egui::Ui;
-use egui_glow::EguiGlow;
-use std::cmp::Ordering;
-use std::fmt::{Display, Formatter, Debug};
-use egui::epaint::ClippedShape;
-use glutin::event::WindowEvent;
-use crate::util::*;
-use std::collections::HashMap;
-use std::any::TypeId;
 
 pub mod frame;
 pub mod buffer;
 pub mod shader;
-pub mod model;
-pub mod generic;
-pub mod test;
 
-///The render contexts makes sure that anything that is .
-
-pub struct RenderContext {
-    pub gl : Context,
-    egui : EguiGlow,
-    render_list : Vec<Box<dyn RenderPass>>
-}
-
-impl RenderContext {
-    pub fn new(window : &ContextWrapper<PossiblyCurrent, Window>, event_loop : &EventLoop<()>) -> Self {
-        unsafe {
-            let gl =
-                glow::Context::from_loader_function(|s| window.get_proc_address(s));
-
-            {
-                use glow::HasContext as _;
-                gl.enable(glow::FRAMEBUFFER_SRGB);
-
-                let mut egui = egui_glow::EguiGlow::new(&window, &gl);
-
-                RenderContext {
-                    gl,
-                    egui,
-                    render_list : Vec::new()
-                }
-            }
-        }
-    }
-
-    pub fn on_event(&mut self, event : &WindowEvent<'_>) {
-        self.egui.on_event(event);
-    }
-
-    pub fn debug(&mut self, window : &ContextWrapper<PossiblyCurrent, Window>) {
-        let (should_render, list) = self.egui.run(window.window(), |egui_ctx| {
-            egui::SidePanel::left("side_panel").show(egui_ctx, |ui|{
-                for render_group in self.render_list.iter_mut() {
-                    if render_group.debugable.is_some() {
-                        ui.collapsing(&render_group.name, |ui| {
-                            render_group.debugable.as_mut().unwrap().debug(ui)
-                        });
-                    }
-                }
-            });
-        });
-
-        self.egui.paint(window, &self.gl, list);
-    }
-
-    pub fn render(&self) {
-        unsafe {
-            self.gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-
-            for render_group in self.render_list.iter() {
-                if render_group.drawable.is_some() {
-                    render_group.drawable.as_ref().unwrap().render(&self.gl);
-                }
-            }
-        }
-    }
-
-    pub fn destroy(&mut self) {
-        unsafe {
-            for render_group in self.render_list.iter() {
-                if render_group.destroyable.is_some() {
-                    render_group.destroyable.as_ref().unwrap().destroy(&self.gl)
-                }
-            }
-
-            self.egui.destroy(&self.gl);
-        }
-    }
-
-    pub fn add_render_group(&mut self, render_group : impl RenderPass) -> usize {
-        self.render_list.push(Box::new(render_group));
-        self.render_list.len() - 1
-    }
-
-    pub fn get_render_group<T>(&self, index : usize) -> &T {
-
-        if TypeId::of::<T>() == self.render_list[index].as_any() {
-
-        }
-        &self.render_list[index]
-    }
-
-    pub fn get_render_group_mut(&mut self, index : usize) -> &mut RenderGroup {
-        &mut self.render_list[index]
-    }
-}
-
-pub enum RenderError {
-    ShaderError,
-}
-
-pub trait RenderPass {
-    unsafe fn draw(&self) -> Result<(), RenderError>;
-
-    fn debug(&mut self) -> Result<(), RenderError> { Ok(())  }
-
-    unsafe fn destroy(&self) -> Result<(), RenderError>;
-}
-
-pub struct RenderGroup {
-    pub priority : u32,
-    pub name : String,
-    pub drawable : Option<Box<dyn Drawable>>,
-    pub debugable : Option<Box<dyn Debugable>>,
-    pub destroyable : Option<Box<dyn Destroyable>>
-}
-
-impl RenderGroup {
-    pub(crate) fn new(name : String, priority : u32) -> Self {
-        RenderGroup {
-            name,
-            priority,
-            drawable : None,
-            debugable : None,
-            destroyable : None
-        }
-    }
-
-    pub fn with_drawable<T: 'static>(mut self, drawable : T) -> Self where T : Drawable {
-        self.drawable = Some(Box::new(drawable));
-        self
-    }
-
-    pub fn with_debugable<T: 'static>(mut self, debugable : T) -> Self where T : Debugable {
-        self.debugable = Some(Box::new(debugable));
-        self
-    }
-
-    pub fn with_destroyable<T: 'static>(mut self, destroyable : T) -> Self where T : Destroyable {
-        self.destroyable = Some(Box::new(destroyable));
-        self
-    }
-
-    pub fn get_name(&self) -> &String {
-        &self.name
-    }
-
-    pub fn get_priority(&self) -> u32 {
-        self.priority
-    }
-}
-
-impl PartialEq for RenderGroup {
-    fn eq(&self, other: &Self) -> bool {
-        self.priority == other.priority
-    }
-}
-
-impl Eq for RenderGroup {}
-
-impl Ord for RenderGroup {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.priority.cmp(&other.priority)
-    }
-}
-
-impl PartialOrd for RenderGroup {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Debug for RenderGroup {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{}:{}]", self.name, self.priority)
-    }
-}
-
-impl Display for RenderGroup {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{}:{}]", self.name, self.priority)
-    }
-}
-
-pub trait Drawable{
+pub trait Renderable {
     unsafe fn render(&self, gl : &Context);
+
+    unsafe fn pre_render(&self, gl : &Context) {
+
+    }
+
+    unsafe fn post_order(&self, gl : &Context) {
+
+    }
 }
 
-pub trait Debugable {
-    fn debug(&mut self, ui : &mut Ui);
-}
-
-pub trait Destroyable {
-    unsafe fn destroy(&self, gl : &Context);
-}
-
-
-
-pub fn createGlutinContext<'a>(title : &str) -> (ContextWrapper<PossiblyCurrent, Window>, EventLoop<()> ) {
+pub fn createGlutinContext<'a>(title : &str) -> (Context, &'a str, ContextWrapper<PossiblyCurrent, Window>, EventLoop<()> ) {
     unsafe {
         let event_loop = glutin::event_loop::EventLoop::with_user_event();
         let window_builder = glutin::window::WindowBuilder::new()
             .with_title(title)
             .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
         let window = glutin::ContextBuilder::new()
+            .with_depth_buffer(0)
             .with_srgb(true)
+            .with_stencil_buffer(0)
             .with_vsync(true)
             .build_windowed(window_builder, &event_loop)
             .unwrap()
             .make_current()
             .unwrap();
 
-        (window, event_loop)
+        let gl =
+            glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
+
+        gl.enable(FRAMEBUFFER_SRGB);
+
+        (gl, "#version 410", window, event_loop)
+    }
+}
+
+pub fn createSurfacelessContext() {
+    unsafe {
+       //todo create a surfaceless context
     }
 }
 

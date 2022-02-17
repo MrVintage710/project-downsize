9bb5c0fca024e2bc1fb769aab6d09da2ac7035ed
Brooks Palin <brookspalin710@gmail.com>
2022-02-17 12:44:27 -0700

    fixed issue with SRGB

--- src/render/texture.rs
+++ src/render/texture.rs
@@ -17,16 +17,16 @@ impl Texture {
                 .expect("Unable to find texutre file.");
 
             let image  = image.decode().unwrap();
-
+            let image = image.into_rgba32f();
             println!("{:?}", image.get_pixel(0, 1));
 
-            let decoder = png::Decoder::new(File::open(format!("assets/textures/{}", filename)).unwrap());
-            let mut reader = decoder.read_info().unwrap();
-
-            let mut buf = vec![0; reader.output_buffer_size()];
-            let info = reader.next_frame(&mut buf).unwrap();
-
-            println!("{}, {}, {}", buf[(info.width * 4) as usize], buf[(info.width * 4 + 1) as usize], buf[(info.width * 4 + 2) as usize]);
+            // let decoder = png::Decoder::new(File::open(format!("assets/textures/{}", filename)).unwrap());
+            // let mut reader = decoder.read_info().unwrap();
+            //
+            // let mut buf = vec![0; reader.output_buffer_size()];
+            // let info = reader.next_frame(&mut buf).unwrap();
+            //
+            // println!("{}, {}, {}", buf[(info.width * 4) as usize], buf[(info.width * 4 + 1) as usize], buf[(info.width * 4 + 2) as usize]);
 
             let texture = gl.create_texture().expect("Can not create texture.");
             gl.bind_texture(TEXTURE_2D, Some(texture));
@@ -34,15 +34,15 @@ impl Texture {
             gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MAG_FILTER, NEAREST as i32);
             gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MIN_FILTER, NEAREST as i32);
 
-            let color_format = match info.color_type {
-                ColorType::Grayscale => {LUMINANCE}
-                ColorType::Rgb => {println!("RGB"); RGB}
-                ColorType::Indexed => {ALPHA}
-                ColorType::GrayscaleAlpha => {LUMINANCE_ALPHA}
-                ColorType::Rgba => {println!("RGBA"); RGBA}
-            };
+            // let color_format = match info.color_type {
+            //     ColorType::Grayscale => {LUMINANCE}
+            //     ColorType::Rgb => {println!("RGB"); RGB}
+            //     ColorType::Indexed => {ALPHA}
+            //     ColorType::GrayscaleAlpha => {LUMINANCE_ALPHA}
+            //     ColorType::Rgba => {println!("RGBA"); RGBA}
+            // };
 
-            gl.tex_image_2d(TEXTURE_2D, 0, color_format as i32, info.width as i32, info.height as i32, 0, color_format, UNSIGNED_BYTE, Some(&buf));
+            gl.tex_image_2d(TEXTURE_2D, 0, SRGB as i32, image.width() as i32, image.height() as i32, 0, RGBA, FLOAT, Some(&image.as_bytes()));
             gl.generate_mipmap(TEXTURE_2D);
 
             Texture{texture}

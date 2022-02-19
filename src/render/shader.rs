c7f18b4697818994d36e20ceb37cbc377087b112
Brooks Palin <brookspalin710@gmail.com>
2022-02-18 23:41:13 -0700

    stopping for the night

--- src/render/shader.rs
+++ src/render/shader.rs
@@ -3,7 +3,7 @@ use std::ops::Add;
 use std::fs;
 use cgmath::{Vector3, Vector2, Vector4, Matrix4};
 use std::collections::HashMap;
-use crate::render::shader::UniformType::{VEC4, VEC3};
+use crate::render::shader::UniformValue::{VEC4, VEC3};
 use crate::render::Debugable;
 use egui::{Ui, DragValue};
 
@@ -15,37 +15,53 @@ const TESSELATION_SHADER_INDEX: usize = 3;
 /// This enum represents all of the types that we can turn into a uniform value. To make something
 /// have the ability to become a uniform, implement `Into<UniformValue>` for that type.
 #[derive(Debug, Clone, Copy)]
-enum UniformType {
+enum UniformValue {
     VEC4(Vector4<f32>),
     VEC3(Vector3<f32>),
     VEC2(Vector2<f32>),
     MAT4(Matrix4<f32>)
 }
 
-/// `Into<UniformType>` implementation for `Vector4<f32>`. This is so that the type can be used in the
+/// `Into<UniformValue>` implementation for `Vector4<f32>`. This is so that the type can be used in the
 /// `create_uniform()` and `set_uniform()` methods.
-impl Into<UniformType> for Vector4<f32> {
-    fn into(self) -> UniformType {
+impl Into<UniformValue> for Vector4<f32> {
+    fn into(self) -> UniformValue {
         VEC4(self)
     }
 }
 
-/// `Into<UniformType>` implementation for `Vector3<f32>`. This is so that the type can be used in the
+/// `Into<UniformValue>` implementation for `Vector3<f32>`. This is so that the type can be used in the
 /// `create_uniform()` and `set_uniform()` methods.
-impl Into<UniformType> for Vector3<f32> {
-    fn into(self) -> UniformType {
+impl Into<UniformValue> for Vector3<f32> {
+    fn into(self) -> UniformValue {
         VEC3(self)
     }
 }
 
-/// This represents a Uniform Variable. Consists of a Uniform Type and a Uniform location.
-type Uniform = (UniformType, UniformLocation);
+/// This represents a Uniform state.
+struct UniformState {
+    current_value : UniformValue,
+    last_value : UniformValue,
+    location : UniformLocation
+}
+
+impl UniformState {
+
+    pub fn new<T>(value : T, location : UniformLocation) -> Self where T : Into<UniformValue> {
+        let value = value.into();
+        UniformState {current_value : value.clone(), last_value : value.clone(), location}
+    }
+
+    pub fn set<T>(&mut self, value : T) where T : Into<UniformValue> {
+        self.current_value = value.into()
+    }
+}
 
 /// This is a shader program struct. It stores all the functionality need for loading shaders and
 /// setting uniform values. This struct also stores the current state of uniforms.
 pub struct ShaderProgram {
     program : NativeProgram,
-    uniforms : HashMap<String, Uniform>,
+    uniforms : HashMap<String, UniformState>,
     shaders : [Option<NativeShader>; 4]
 }
 
@@ -106,7 +122,7 @@ impl ShaderProgram {
         }
     }
 
-    pub fn uniform<T>(&mut self, gl : &Context, name : &str, value : T) where T : Into<UniformType>{
+    pub fn uniform<T>(&mut self, gl : &Context, name : &str, value : T) where T : Into<UniformValue>{
         unsafe {
             self.bind(gl);
             let (uniform_type, uniform_location) = if !self.uniforms.contains_key(name) {
@@ -124,8 +140,8 @@ impl ShaderProgram {
             match uniform_type {
                 VEC4(vec) => {todo!()}
                 VEC3(vec) => gl.uniform_3_f32(Some(&uniform_location), vec.x, vec.y, vec.z),
-                UniformType::VEC2(vec) => {todo!()}
-                UniformType::MAT4(vec) => {todo!()}
+                UniformValue::VEC2(vec) => {todo!()}
+                UniformValue::MAT4(vec) => {todo!()}
             }
             self.uniforms.insert(name.to_string(), (uniform_type, uniform_location));
         }
@@ -168,8 +184,8 @@ impl Debugable for ShaderProgram {
                         ui.add(DragValue::new(&mut vec.x));
                     });
                 }
-                UniformType::VEC2(vec) => {todo!()}
-                UniformType::MAT4(mat) => {todo!()}
+                UniformValue::VEC2(vec) => {todo!()}
+                UniformValue::MAT4(mat) => {todo!()}
             }
         }
     }

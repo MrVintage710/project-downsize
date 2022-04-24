55585bbdd2949129639d2dc0d65206fcec71098e
Brooks Palin <brookspalin710@gmail.com>
2022-04-24 00:44:02 -0600

    Reverted shader from last commit

--- assets/shaders/static_frag.glsl
+++ assets/shaders/static_frag.glsl
@@ -5,14 +5,9 @@ in vec4 vertex_color;
 in vec2 uv_pos;
 
 uniform sampler2D our_texture;
-
-//Global Lighting
-uniform float ambient_min;
-uniform vec3 global_lighting_color;
+uniform vec3 color_shift;
 
 void main() {
-    vec3 ambient = ambient_min * global_lighting_color;
-
-    vec3 result_color = texture(our_texture, uv_pos) * ambient;
-    frag_color = vec4(result_color, 1.0);
+    //    frag_color = vec4(1.0, 1.0, 1.0, 1.0);
+    frag_color = texture(our_texture, uv_pos) + vec4(color_shift, 1.0);
 }
